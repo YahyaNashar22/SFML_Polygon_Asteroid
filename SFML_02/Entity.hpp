5f9cad6f72463b52a61e7907d1821e2435550bfa
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2026-04-28 10:37:52 +0300

    added EntityManager initial setup

--- SFML_02/Entity.hpp
+++ SFML_02/Entity.hpp
@@ -1,5 +1,3 @@
-#pragma once
-
 #include <string>
 #include <tuple>
 
@@ -8,51 +6,57 @@
 class EntityManager;
 
 using ComponentTuple =
-	std::tuple<CTransform, CShape, CCollision, CInput, CScore, CLifeSpan,
-			   // TODO add the special power component here
-			   >;
+    std::tuple<CTransform, CShape, CCollision, CInput, CScore, CLifeSpan,
+	       // TODO add the special power component here
+	       >;
 
-class Entity {
+class Entity
+{
   friend class EntityManager;
 
   ComponentTuple m_components;
-  bool			 m_active = true;
+  bool		 m_active = true;
   std::string	 m_tag	  = "default";
-  size_t		 m_id	  = 0;
+  size_t	 m_id	  = 0;
 
   Entity(const size_t& id, const std::string& tag) : m_tag(tag), m_id(id) {}
 
  public:
-  bool				 isActive() const { return m_active; }
-  void				 destroy() { m_active = false; }
-  size_t			 id() const { return m_id; }
+  bool		     isActive() const { return m_active; }
+  void		     destroy() { m_active = false; }
+  size_t	     id() const { return m_id; }
   const std::string& tag() const { return m_tag; }
 
   template <typename T>
-  bool has() const {
-	return get<T>().exists;
+  bool has() const
+  {
+    return get<T>().exists;
   }
 
   template <typename T, typename... TArgs>
-  T& add(TArgs&&... mArgs) {
-	auto& component	 = get<T>();
-	component		 = T(std::forward<TArgs>(mArgs)...);
-	component.exists = true;
-	return component;
+  T& add(TArgs&&... mArgs)
+  {
+    auto& component  = get<T>();
+    component	     = T(std::forward<TArgs>(mArgs)...);
+    component.exists = true;
+    return component;
   }
 
   template <typename T>
-  T& get() {
-	return std::get<T>(m_components);
+  T& get()
+  {
+    return std::get<T>(m_components);
   }
 
   template <typename T>
-  const T& get() const {
-	return std::get<T>(m_components);
+  const T& get() const
+  {
+    return std::get<T>(m_components);
   }
 
   template <typename T>
-  void remove() {
-	get<T>() = T();
+  void remove()
+  {
+    get<T>() = T();
   }
 };
